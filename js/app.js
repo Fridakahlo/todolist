758dc71374866ab38213cdae41759c60eb85f6d9
jordan <jordan.lefort@laposte.net>
2016-10-11 16:35:51 +0200

    ajout du localStorage

--- js/app.js
+++ js/app.js
@@ -1,16 +1,62 @@
-// (function(){
+
 
 
 	"use strict";
 
-	var tabValue= [];
+
+	var tabVal = localStorage.tabValue;
+
+
+	
+	if(localStorage.tabValue === undefined){
+
+		var tabVal = [];
+		
+		var tab = JSON.stringify(tabVal);
+		localStorage.setItem("tabValue",tab);
+
+	}
+
+	var tabValue = [];
+
+	for(var i=0;i < (JSON.parse(localStorage.tabValue)).length;i++){
+
+
+
+		tabValue.push(JSON.parse(localStorage.tabValue)[i]);
+
+
+	}
+
+
+
+
+
+	for(var i = 0; i < tabValue.length;i++){
+
+		if(tabValue[i].status === false){
+
+			console.log('false')
+
+			$('#affichList').append("<div  id='div"+tabValue[i].number+"' class='tache'><input class='check' id='check"+tabValue[i].number+"' data-object='"+tabValue[i].number+"'  type='checkbox'><div  id='"+tabValue[i].number+"' class='tacheContent'>"+tabValue[i].value+"</div></div>");
+			
+		}
+
+		if(tabValue[i].status === true){
+
+			console.log('true')
+
+			$('#affichList').append("<div  id='div"+tabValue[i].number+"' class='tache'><input class='check' id='check"+tabValue[i].number+"' data-object='"+tabValue[i].number+"' type='checkbox' checked><div  id='"+tabValue[i].number+"' class='tacheContent'>"+tabValue[i].value+"</div></div>");
+			$("#"+tabValue[i].number).addClass('rayer');
+		}
+	}	
+
 
 
 	$('#createdTache').click(function(){
-	
-			
 
-		
+	
+	
 
 
 		var todo = {
@@ -18,29 +64,33 @@
 			value : $('#newTache').val(),
 			status : false,
 			nameList : null,
-			id : 'div'+(tabValue.length+1),
-			
-
+			id : 'div'+((JSON.parse(localStorage.tabValue)).length+1),
+			number : ((JSON.parse(localStorage.tabValue)).length+1),
 
 		}
+
 
 
 		if($('#newTache').val()){
-		 
+
 			tabValue.push(todo);
-			$('#affichList').append("<div  id='div"+tabValue.length+"' class='tache'><input class='check' id='check"+tabValue.length+"'data-object='"+tabValue.length+"' type='checkbox'><div  id='"+tabValue.length+"' class='tacheContent'>"+tabValue[tabValue.length-1].value+"</div></div>");
+
+			var tab = JSON.stringify(tabValue);
+			localStorage.setItem("tabValue",tab);
+
+			$('#affichList').append("<div  id='div"+(JSON.parse(localStorage.tabValue)).length+"' class='tache'><input class='check' id='check"+(JSON.parse(localStorage.tabValue)).length+"'data-object='"+(JSON.parse(localStorage.tabValue)).length+"' type='checkbox'><div  id='"+(JSON.parse(localStorage.tabValue)).length+"' class='tacheContent'>"+(JSON.parse(localStorage.tabValue))[(JSON.parse(localStorage.tabValue)).length-1].value+"</div></div>");
+
+
 		}
- 
+
+			console.log(tabVal);
+	
 		$('.check').click(function(){
-
-
 
 			if(checked("#check"+$(this).data("object"))){
 
-
 				tabValue[$(this).data("object")-1].status = true ;
 				$("#"+$(this).data("object")).addClass('rayer');
-
 
 			}
 
@@ -48,17 +98,16 @@
 
 				tabValue[$(this).data("object")-1].status = false ;
 				$("#"+$(this).data("object")).removeClass('rayer');
-
-
 			}
-
 
 		});
 
 
+		
 
+	});
 
-		$("#checkEffect").click(function(){
+	$("#checkEffect").click(function(){
 
 			for(var i =0;i < tabValue.length;i++){
 
@@ -66,21 +115,17 @@
 
 					$('#'+tabValue[i].id).addClass('displayNone');
 
-				
-
 				}
+
 				if (tabValue[i].status === true){
 
 					$('#'+tabValue[i].id).removeClass('displayNone');
 
 				};
-
 			}
 
-		
-
-
 		});
+
 
 		$("#checkAll").click(function(){
 
@@ -88,11 +133,10 @@
 
 				$('#'+tabValue[i].id).removeClass('displayNone');
 
-				
 			}
 
-			
 		});
+
 
 		$("#checkTodo").click(function(){
 
@@ -110,49 +154,40 @@
 
 			}
 
-		
+		});
 
+		$('.check').click(function(){
+
+			console.log($(this).data("object"));
+
+			if(checked("#check"+$(this).data("object"))){
+
+				tabValue[$(this).data("object")-1].status = true ;
+				$("#"+$(this).data("object")).addClass('rayer');
+
+				var tab = JSON.stringify(tabValue);
+				localStorage.setItem("tabValue",tab);
+
+
+			}
+
+			if(!checked("#check"+$(this).data("object"))){
+
+				tabValue[$(this).data("object")-1].status = false ;
+				$("#"+$(this).data("object")).removeClass('rayer');
+
+				var tab = JSON.stringify(tabValue);
+				localStorage.setItem("tabValue",tab);
+			}
 
 		});
 
 
-	});
-
-
-	
-
-
-
-
-
-	
-
-
-
-
-	
-
 	function checked (check){
-		return $(check).is(":checked");
+	return $(check).is(":checked");
 
 	}
 
 
-
-	
-
-	
-
-
-	
-
-
-
-
-
-
-
-
-// })();
 
 
